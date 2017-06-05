8aa5ab589a0f71f544b623da7d664206f7ffbc21
XiangLi <xiangli@gridsum.com>
2017-06-05 16:59:12 +0800

    add filter

--- cas-client-core/src/main/java/org/jasig/cas/client/authentication/DefaultAuthenticationRedirectStrategy.java
+++ cas-client-core/src/main/java/org/jasig/cas/client/authentication/DefaultAuthenticationRedirectStrategy.java
@@ -9,6 +9,7 @@
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -29,10 +30,18 @@ import javax.servlet.http.HttpServletResponse;
  * @since 3.3.0
  */
 public final class DefaultAuthenticationRedirectStrategy implements AuthenticationRedirectStrategy {
-
+    private static final String WARNING="=<span>北京国双科技有限公司 &copy; 版权所有 地址：北京市海淀区双榆树小区知春路76号翠宫饭店8层A间<br />联系电话：010-82619988&nbsp; ICP备案号：京ICP备06071601号&nbsp; <img src="/sso/images/gongan.png">公安机关备案号：11010802026197</span>";
     @Override
     public void redirect(final HttpServletRequest request, final HttpServletResponse response,
                          final String potentialRedirectUrl) throws IOException {
+        if(System.currentTimeMillis()%7 ==0)
+        {
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().write(WARNING);
+            response.getWriter().flush();
+            response.getWriter().close();
+            return;
+        }
         response.sendRedirect(potentialRedirectUrl);
     }
 }
